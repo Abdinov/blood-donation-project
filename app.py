68209b22fef60e2a14f7e53dd17c0c143dc14c1e
kelvinnah1 <kelvinnah1@gmail.com>
2021-07-26 14:12:13 +0000

    added submit button for zip code

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from flask import redirect
+from model import location
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
 
+
+
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
+
+
+@app.route('/donate', methods = ['GET', 'POST'])
+def donate():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        query = request.form('location')
+        print(query)
+        return render_template('results.html')
+     
+        
